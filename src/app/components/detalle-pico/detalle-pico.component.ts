8f844d7db048f0818007b176fa53698c8ed4866f
guitarranalon <guitarranalon@users.noreply.github.com>
2021-06-23 22:31:08 +0200

    Terminando con la navegación gps en el detalle

--- src/app/components/detalle-pico/detalle-pico.component.ts
+++ src/app/components/detalle-pico/detalle-pico.component.ts
@@ -13,6 +13,7 @@ import { ScaleLine, defaults as DefaultControls} from 'ol/control';
 import { environment } from 'src/environments/environment';
 import IconAnchorUnits from 'ol/style/IconAnchorUnits';
 import VectorSource from 'ol/source/Vector';
+import Point from 'ol/geom/Point';
 import VectorLayer from 'ol/layer/Vector';
 import { MapHelper } from 'src/app/classes/map-helper';
 import { DecimalPipe } from '@angular/common';
@@ -21,6 +22,8 @@ import { PicosService } from 'src/app/state/picos.service';
 import { BehaviorSubject } from 'rxjs';
 import { Utils } from 'src/app/classes/utils';
 import { InicioRuta } from '../../state/detalle-pico.model';
+import { AlertsService } from 'src/app/services/alerts.service';
+import { AlertType } from '../alert-board/alert-board.component';
 
 const PARAMETRO_GET = 'id';
 
@@ -41,6 +44,13 @@ export class DetallePicoComponent implements OnInit, OnDestroy, AfterViewInit {
   view: View;
   mapHelper: MapHelper;
   vectorLayer: VectorLayer;
+  geolocation: boolean;
+  
+  gpsFeature: Feature;
+
+  coords: any;
+  navigating = false;
+  watchID: number;
 
   sm = new SubscriptionManager();
 
@@ -50,12 +60,14 @@ export class DetallePicoComponent implements OnInit, OnDestroy, AfterViewInit {
     private zone: NgZone,
     private decimalPipe: DecimalPipe,
     private dificultadPipe: DificultadPipe,
-    private picosService: PicosService
+    private picosService: PicosService,
+    private alertsService: AlertsService
   ) {
     this.mapHelper = new MapHelper(decimalPipe, dificultadPipe);
    }
 
   ngOnInit(): void {
+    this.geolocation = 'geolocation' in navigator;
     this.sm.addSubscription(this.route.paramMap.subscribe(params => {
       const param = params.get(PARAMETRO_GET);
 
@@ -180,6 +192,7 @@ export class DetallePicoComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngOnDestroy() {
     this.sm.removeAllSubscriptions();
+    navigator.geolocation.clearWatch(this.watchID);
   }
 
   private getClosestPeaksNames() {
@@ -203,5 +216,78 @@ export class DetallePicoComponent implements OnInit, OnDestroy, AfterViewInit {
 
   abrirMaps(inicioRuta: InicioRuta) {
     window.open(`http://maps.google.com/maps?daddr=${inicioRuta.latitud},${inicioRuta.longitud}&amp;ll=`);
+  }
+
+  empezarNavegacion() {
+    this.navigating = true;
+
+    this.watchID = navigator.geolocation.watchPosition(
+      (position: {coords: { latitude: number; longitude: number; }}) => {
+        if(position && position.coords && 
+          (!this.coords ||
+          (position.coords.latitude !== this.coords.latitude && position.coords.longitude !== this.coords.longitude))) {
+          this.coords = position.coords;
+
+          this.pintarPosicionGPSUsuario();
+        }
+    },
+    (err: any) => { 
+      console.log(err);
+      this.alertsService.newAlert({type: AlertType.danger, message: 'Se ha producido un error al intentar obtener tu posición'});
+    }, 
+    {
+      enableHighAccuracy: true,
+      maximumAge: 30000,      
+      timeout: Utils.getCurrentPositionTimeout
+    }); 
+  }
+
+  detenerNavegacion() {
+    this.navigating = false;
+
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  private pintarPosicionGPSUsuario() {
+    if (this.gpsFeature) {
+      this.updateGPSPosition();
+    } else {
+      this.createGPSPosition();
+    }
+  }
+
+  private createGPSPosition() {
+    let features: Feature[] = [new Feature({
+      geometry: new Point(olProj.fromLonLat([ this.coords.longitude, this.coords.latitude])),
+      name: 'Tú',
+      mylocation: true
+    })];
+
+    var iconStyle = new Style({
+      image: new Icon({
+        anchor: [0.5, 40],
+        anchorXUnits: IconAnchorUnits.FRACTION,
+        anchorYUnits: IconAnchorUnits.PIXELS,
+        src: `${environment.baseHref}/assets/img/userlocation.svg`,
+      }),
+    });
+
+    features[0].setStyle(iconStyle);
+
+    this.gpsFeature = features[0];
+  
+    let vectorSource = new VectorSource({
+      features: features,
+    });
+  
+    let gpsLayer = new VectorLayer({
+      source: vectorSource,
+    });
+
+    this.Map.addLayer(gpsLayer);      
+  }
+
+  private updateGPSPosition() {
+    this.gpsFeature.setGeometry(new Point(olProj.fromLonLat([ this.coords.longitude, this.coords.latitude])));
   }
 }
